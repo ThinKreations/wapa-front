e371f0a9cd1ea6d163986636cd2c017b6e9757e8
ThinKreations <napoles.munguia.josedejesus.1@gmail.com>
2025-11-15 02:31:11 -0600

    Vista de Login

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import "@/styles/Fonts.css"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head/>
       <body>
         <Main />
         <NextScript />
---
+++ pages/sesion/recuperar.js
@@ -0,0 +1,12 @@
+import MainHead from "@/components/MainHead";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+        <MainHead title="Recuperar"/>
+    </>
+    )
+}
